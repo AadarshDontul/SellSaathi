1442f1cf698aa5c0280e76e9ea1819a88ee1116d
Aadarsh-D <aadarshdontul03@gmail.com>
2024-12-15 19:47:25 +0530

    Initialised the SellSaathi Application

---
+++ src/main/java/com/ethane3/sellsaathi/SellSaathiApplication.java
@@ -0,0 +1,13 @@
+package com.ethane3.sellsaathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellSaathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellSaathiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ethane3/sellsaathi/SellSaathiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethane3.sellsaathi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellSaathiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
